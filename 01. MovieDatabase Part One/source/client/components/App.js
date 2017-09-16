d01135e4a3bbd60918131a3cb02e81ae82c15bb8
Georgi Stankov <vangog06406@gmail.com>
2017-09-16 13:07:14 +0300

    Movie Database
    This is the first part of the Movie Database

---
+++ 01. MovieDatabase Part One/source/client/components/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedInUserId: ''
+    };
+
+    this.LOGIN_DEFAULT_USER = this.LOGIN_DEFAULT_USER.bind(this);
+  }
+
+  componentDidMount() {
+    this.LOGIN_DEFAULT_USER();
+  }
+
+  LOGIN_DEFAULT_USER() {
+    let request = {
+      method: 'post',
+      url: 'user/login',
+      data: JSON.stringify({
+        username: 'admin',
+        password: 'admin'
+      }),
+      contentType: 'application/json'
+    };
+
+    $.ajax(request)
+      .done(userId => {
+        this.setState({
+          loggedInUserId: userId
+        });
+      })
+      .fail(err => {
+        console.log('UserMenu: err', err);
+        this.setState({
+          loggedInUserId: '',
+          message: err.responseJSON.mssage
+        });
+      });
+  }
+
+  logoutUser() {
+    let request = {
+      method: 'post',
+      url: '/user/logout'
+    };
+
+    $.ajax(request)
+      .done(() => {
+        this.setState({
+          loggedInUserId: ''
+        });
+      })
+      .fail(err => {
+        this.setState({
+          error: err.responseJSON.message
+        });
+      });
+  }
+
+  render() {
+    let userData = {
+      loggedInUserId: this.state.loggedInUserId,
+      loginUser: this.LOGIN_DEFAULT_USER,
+      logoutUser: this.logoutUser.bind(this)
+    };
+
+    return (
+      <div>
+        <Navbar history={this.props.history} userData={userData} />
+        { this.props.children }
+        <Footer />
+      </div>
+    );
+  }
+}
---
+++ 01. MovieDatabase Part One/source/client/components/Home.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import MovieCard from "./sub-components/MovieCard";
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topTenMovies: [],
+      error: ''
+    };
+  }
+
+  componentDidMount() {
+    let request = {
+      method: 'get',
+      url: '/api/movies/top-ten'
+    };
+
+    $.ajax(request)
+      .done(data => this.setState({ topTenMovies: data }))
+      .fail(err => this.setState({ error: error.responseJSON.message }))
+  }
+
+  render() {
+    let movies = this.state.topTenMovies.map((movie, index) => {
+      return (
+        <MovieCard key={ movie._id }
+                   movie={ movie }
+                   index={ index } />
+      );
+    });
+
+    return (
+      <div className="container">
+        <h3 className="text-center">Welcome to
+          <strong> Movie Database</strong>
+        </h3>
+        <div className="list-group">
+          {movies}
+        </div>
+      </div>
+    );
+  }
+}
